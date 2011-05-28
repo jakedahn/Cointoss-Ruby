31f93f5398fa6bf99666b2232c5ea1746df13c8b
Jake Dahn <admin@jakedahn.com>
2011-05-27 23:20:09 -0700

    refactoring to make it not look horrible

--- cointoss.rb
+++ cointoss.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'sinatra'
-require 'nokogiri'
 require 'open-uri'
 
 set :public, File.dirname(__FILE__) + '/static'
@@ -10,27 +9,16 @@ get '/' do
 end
 
 get '/toss' do
-  doc = Nokogiri::HTML(open('http://www.random.org/integers/?num=10000&min=0&max=1&col=1&base=10&format=html&rnd=new'))
+  results = open('http://www.random.org/integers/?num=10&min=0&max=1&col=1&base=10&format=plain&rnd=new').string
 
-  def getCount(a, h)
-    a.each { |v| h[v] += 1 }
-    h
-  end
-
-  def getSide(heads, tails)
-    case heads <=> tails
+  @heads = results.count("1")
+  @tails = results.count("0")
+  
+  @side = case @heads <=> @tails
     when +1: "Heads."
     when  0: "Tie."
     when -1: "Tails."
-    end
   end
-
-  count = getCount(doc.css('pre.data').inner_text.split, Hash.new(0))
-
-  @heads = count["0"]
-  @tails = count["1"]
-
-  @side = getSide(@heads, @tails)
 
   erb :toss
 end
