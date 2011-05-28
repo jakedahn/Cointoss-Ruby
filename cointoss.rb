af94d4a9ef9ea9ab3d41307702c9959c19fd85a1
Jake Dahn <admin@jakedahn.com>
2011-05-27 23:48:02 -0700

    redid css

--- cointoss.rb
+++ cointoss.rb
@@ -9,10 +9,11 @@ get '/' do
 end
 
 get '/toss' do
-  results = open('http://www.random.org/integers/?num=10&min=0&max=1&col=1&base=10&format=plain&rnd=new').string
+  results = open('http://www.random.org/integers/?num=10&min=0&max=1&col=1&base=10&format=plain&rnd=new').string.chop
 
   @heads = results.count("1")
   @tails = results.count("0")
+  @last = results[-1, 1] == "1" ? "Heads" : "Tails"
   
   @side = case @heads <=> @tails
     when +1: "Heads."
